d12244aa956ab9e762d0b438079cacad61b7effc
Aje Success <73680210+successaje@users.noreply.github.com>
2022-05-27 20:18:07 +0100

    update

--- dapp/src/App.tsx
+++ dapp/src/App.tsx
@@ -12,22 +12,27 @@ function App() {
   const [wallet, setWallet] = useState<any>(null);
   const [userAddress, setUserAddress] = useState<string>("");
   const [userBalance, setUserBalance] = useState<number>(0);
-
+  
+  const [contractToPoke, setContractToPoke] = useState<string>("");
   const contractsService = new ContractsService( {baseUrl: "https://api.ithacanet.tzkt.io" , version : "", withCredentials : false});
   const [contracts, setContracts] = useState<Array<Contract>>([]);
 
   const fetchContracts = () => {
     (async () => {
-     setContracts((await contractsService.getSimilar({address:"KT1M1sXXUYdLvow9J4tYcDDrYa6aKn3k1NT9" , includeStorage:true, sort:{desc:"id"}})));
+     setContracts((await contractsService.getSimilar({address:"KT1QzjERRAq7rvyRwpXRjHmeR2YUpUacpEyM" , includeStorage:true, sort:{desc:"id"}})));
     })();
   }
 
-  const poke = async (contract : Contract) => {   
+   //poke
+   const poke = async (e :  React.FormEvent<HTMLFormElement>, contract : Contract) => {  
+    e.preventDefault(); 
     let c : WalletContract = await Tezos.wallet.at(""+contract.address);
     try {
-      const op = await c.methods.default().send();
+      const op = await c.methods.pokeAndGetFeedback(contractToPoke).send();
       await op.confirmation();
+      alert("Tx done");
     } catch (error : any) {
+      console.log(error);
       console.table(`Error: ${JSON.stringify(error, null, 2)}`);
     }
   };
@@ -59,8 +64,8 @@ function App() {
         <br />
     <div>
     <button onClick={fetchContracts}>Fetch contracts</button>
-    <table><thead><tr><th>address</th><th>people</th><th>action</th></tr></thead><tbody>
-    {contracts.map((contract) => <tr><td style={{borderStyle: "dotted"}}>{contract.address}</td><td style={{borderStyle: "dotted"}}>{contract.storage.join(", ")}</td><td style={{borderStyle: "dotted"}}><button onClick={() =>poke(contract)}>Poke</button></td></tr>)}
+    <table><thead><tr><th>address</th><th>trace "contract - feedback - user"</th><th>action</th></tr></thead><tbody>
+    {contracts.map((contract) => <tr><td style={{borderStyle: "dotted"}}>{contract.address}</td><td style={{borderStyle: "dotted"}}>{(contract.storage !== null && contract.storage.pokeTraces !== null && Object.entries(contract.storage.pokeTraces).length > 0)?Object.keys(contract.storage.pokeTraces).map((k : string)=>contract.storage.pokeTraces[k].receiver+" "+contract.storage.pokeTraces[k].feedback+" "+k+","):""}</td><td style={{borderStyle: "dotted"}}><form onSubmit={(e) =>poke(e,contract)}><input type="text" onChange={e=>setContractToPoke(e.currentTarget.value)} placeholder='enter contract address here' /><button  type='submit'>Poke</button></form></td></tr>)}
     </tbody></table>
     </div>
 
